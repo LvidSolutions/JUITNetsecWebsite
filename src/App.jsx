b1cf4a3de00ed09f459d75d618da7758cbad1589
LvidSolutions <solutions@wwwlvid.com>
2026-06-13 19:47:30 +0200

    Visa referenser CTA och kontakt

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { Header } from './components/layout/Header.jsx';
 import { AboutSection } from './components/sections/AboutSection.jsx';
+import { ContactSection } from './components/sections/ContactSection.jsx';
+import { CtaSection } from './components/sections/CtaSection.jsx';
 import { Hero } from './components/sections/Hero.jsx';
+import { ReferencesSection } from './components/sections/ReferencesSection.jsx';
 import { ServicesSection } from './components/sections/ServicesSection.jsx';
 import { TrustSection } from './components/sections/TrustSection.jsx';
 
@@ -12,6 +15,9 @@ function App() {
       <TrustSection />
       <ServicesSection />
       <AboutSection />
+      <ReferencesSection />
+      <CtaSection />
+      <ContactSection />
     </main>
   );
 }
